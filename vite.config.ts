9a1ad0529449f22f0bc95110fefcd5a2458b641c
Takendra Bam <takendrabam051@gmail.com>
2025-11-02 21:53:49 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // important for GitHub Pages and custom domains
   server: {
     host: "::",
     port: 8080,
